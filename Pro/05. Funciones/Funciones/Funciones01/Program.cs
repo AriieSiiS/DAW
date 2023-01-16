ca67bd47ad90885513c0a8924fd724a171a617cb
Lord Ariesis <ariesix20@gmail.com>
2023-01-16 17:11:28 +0000

    Ejercicios PRORecursividad 1-2-4

---
+++ Pro/05. Funciones/Funciones/Funciones01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones01
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Dime una temperatura en grados Celcius");
+            while (!(Int32.TryParse(Console.ReadLine(), out num) || num > 0))
+                Console.WriteLine("El número introducido no es válido");
+            Funciones.Fahrenheit(num);
+            Funciones.Kelvin(num);
+        }
+
+        class Funciones
+        {
+            public static void Fahrenheit(int num)
+            {
+               
+                Console.WriteLine("La temperatura en Fahrenheit sería: \n \t {0}ºF", num * 1.8 + 32);
+            }
+
+            public static void Kelvin(int num)
+            {
+                Console.WriteLine("La temperatura en Kelvin sería: \n \t {0}ºK", num + 273.15);
+            }
+        }
+
+    }
+
+
+
+}
---
+++ Pro/05. Funciones/Funciones/Funciones04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Dime una frase");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Ahora dime una letra");
+            char letra = Console.ReadLine()[0];
+            Program.Cantidad(frase,letra);
+        }
+
+        public static void Cantidad(string frase, char letra)
+        {
+            int contador = 0;   
+            foreach (char letra1 in frase)
+            {
+                if (letra1 == letra) 
+                    contador++;
+            }
+            Console.WriteLine("El total de veces que aparece la letra {0} en la frase es {1}",letra,contador);
+        }
+    }
+}
