91143bd5033c0a27e32674ea82ef576e88872842
Lord Ariesis <ariesix20@gmail.com>
2023-02-13 22:39:16 +0000

    Test

---
+++ Pro/06.1 Examen Ficheros Test/Test01/Test01/CSMercancias.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class CSMercancias
+    {
+        public static string ComprobarDestinos()
+        {
+            string Destino = "";
+            while (Destino == "")
+            {
+                Destino = Console.ReadLine();
+                if (Destino == "")
+                    Console.WriteLine("El destino no puede estar vacío, intentalo de nuevo.");
+            }
+            return Destino;
+        }
+
+        public static decimal ComprobarPesos()
+        {
+            Console.WriteLine("Ahora dime el peso de la mercancía");
+            decimal peso;
+            while (!(Decimal.TryParse(Console.ReadLine(), out peso)) || (peso > 1200) || (peso < 0))
+            {
+                Console.WriteLine("El peso introducido no es correcto, intentalo de nuevo.");
+            }
+            return peso;
+       
+        }
+
+        public static void MostrarDatos(string[] destinos, decimal[] pesos)
+        {
+            Console.Clear();
+            for (int i = 0; i < destinos.Length; i++)
+            {
+                Console.WriteLine("El destino {0} lleva {1}kg de peso.", destinos[i], pesos[i]);
+            }
+        }
+    }
+}
---
+++ Pro/06.1 Examen Ficheros Test/Test01/Test01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio
+{
+    internal class Ejercicio
+    {
+        static void Main(String[] args)
+        {
+            //primera parte
+            string[] destinos = new string[0];
+            decimal[] pesos = new decimal[0];
+            string destino = "";
+            int contador = 0;
+            while (destino != "0")
+            {
+                Console.WriteLine("Dime el nombre del destino o escribe 0 para salir");
+                destino = CSMercancias.ComprobarDestinos();
+                if (destino != "0")
+                {
+                    Array.Resize(ref destinos, destinos.Length+1);
+                    Array.Resize(ref pesos, pesos.Length + 1);
+                    destinos[contador] = destino;
+                    decimal peso = CSMercancias.ComprobarPesos();
+                    pesos[contador] = peso;
+                    contador++;
+                }
+            }
+            CSMercancias.MostrarDatos(destinos, pesos);
+            //segunda parte
+            List<string> destinosContenedores = CSContenedores.DestinosContenedores(destinos);
+        }
+    }
+}
+
+
