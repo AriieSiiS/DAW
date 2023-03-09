af9b5d0cc6cf11e36f3fb5aa6dca981cc2b1cb53
Alejandro Afonso Barber <ariesix20@gmail.com>
2023-03-09 13:48:37 +0000

    ETS Proyecto Parejas

---
+++ Pro/EnParejas/ejercicio 1/CSFunciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPP
+{
+    public struct People
+    {
+        public string name;
+        public string surname;
+        public decimal height;
+    }
+    internal class CSFunciones
+    {
+        public static int SolicitarNumeroPersonas()
+        {
+            int countPeople;
+            Console.WriteLine("¿Cuántos alumnos hay en la clase?");
+            while(!Int32.TryParse(Console.ReadLine(),out countPeople) || !(countPeople > 0))
+                Console.WriteLine("Lo que has insertado no es un valor numerico valido");
+            return countPeople;
+        }
+         public static void LeerDatosMuestra(People[] classrom)
+         {
+
+            for(int i=0;i<classrom.Length; i++)
+            {
+                Console.WriteLine("Introduce el nombre del {0}º alumno:",i+1);
+                while (((classrom[i].name = Console.ReadLine())==" ") &&(classrom[i].name!=""))
+                    Console.WriteLine("La cadena no puede estar vacía");
+                Console.WriteLine("Introduce su apellido:");
+                while(((classrom[i].surname=Console.ReadLine())==" ") && (classrom[i].surname==""))
+                    Console.WriteLine("La cadena no puede estar vacía");
+                Console.WriteLine("¿Cuánto mide el alumno?");
+                while(!(decimal.TryParse(Console.ReadLine(),out classrom[i].height))||!(classrom[i].height<3)||!(classrom[i].height>0))
+                    Console.WriteLine("El valor no se encuentra dentro de los parámetros requeridos");
+            }
+          }
+         public static void MostrarDatosMuestra(People[] classrom)
+          {
+            foreach(People alumnado in classrom )
+            {
+                Console.WriteLine("Nombre:{0} {1} \tEstatura:{2:f2}",alumnado.name,alumnado.surname,alumnado.height);
+            }
+          }   
+     }
+}
---
+++ Pro/EnParejas/ejercicio 1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EjercicioPP
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Necesarios 
+            string msg;
+            //Perdir número de alumnos 
+            int countPeople=CSFunciones.SolicitarNumeroPersonas();
+            //Crear la clase 
+            People[] classroom = new People[countPeople];
+            //Inicializamos la clase
+            CSFunciones.LeerDatosMuestra(classroom);
+            //Crear los tamaños 
+            People[] tallStudents=new People[0];
+            People[] smallStudents=new People[0];
+            People[] equalAvgStudents=new People[0];
+            //Crear y inicializar media
+            decimal average = CSAlturas.CalcularMedia(classroom);
+            //Inicializar tamaños
+            CSAlturas.PersonasPorEncimaMedia(classroom, ref tallStudents, average);
+            CSAlturas.PersonasPorDebajoMedia(classroom, ref smallStudents, average);
+            CSAlturas.PersonasIgualMedia(classroom, ref equalAvgStudents, average);
+            //Mostrar alumnos en total 
+            CSFunciones.MostrarDatosMuestra(classroom);
+            //Resetear pantalla
+            Console.ReadKey();
+            Console.Clear();
+            //Mostrar menú para elegir opción
+            Console.WriteLine("la media es de {0:f2}", average);
+            Console.WriteLine("Dime una de las siguientes opciones:");
+            Console.WriteLine("Por encima de la media = \"+\"");
+            Console.WriteLine("Por debajo de la media = \"-\"");
+            Console.WriteLine("Igual a la media = \"=\"");
+            Console.WriteLine("Recuerde usar solo símbolos");
+            //Elegir opción
+            while (!((msg = Console.ReadLine()) == "+") && !(msg == "-") && !(msg == "="))
+                Console.WriteLine("Solo puede ser los símbolos indicados arriba");
+            //Mostrar por pantalla la elección
+            CSAlturas.MostrarPersonas(msg,smallStudents,tallStudents,equalAvgStudents);
+        }
+    }
+}
