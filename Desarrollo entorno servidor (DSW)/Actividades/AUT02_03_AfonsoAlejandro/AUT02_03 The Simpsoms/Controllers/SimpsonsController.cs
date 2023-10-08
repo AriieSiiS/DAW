11d5c22fcbe92af50e383a5d649540f6659f8eab
AriieSiiS <ariesix20@gmail.com>
2023-10-08 17:45:13 +0100

    AUT02_03 - DSW
    - Actividad AUT02_03 The Simpsons meets MVC de DSW

---
+++ Desarrollo entorno servidor (DSW)/Actividades/AUT02_03_AfonsoAlejandro/AUT02_03 The Simpsoms/Controllers/SimpsonsController.cs
@@ -0,0 +1,95 @@
+﻿using AUT02_03_The_Simpsoms.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+namespace AUT02_03_The_Simpsoms.Controllers
+{
+    public class SimpsonsController : Controller
+    {
+
+        static int currentId = 3;
+        static List<Character> simpsonsCharacters = new List<Character>
+        {
+            new Character { ID = 1, Name = "Homer Simpson", Age = 40, Job = "Planta Nuclear" },
+            new Character { ID = 2, Name = "Marge Simpson", Age = 38, Job = "Ama de casa" }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CharacterList()
+        {
+            return View(simpsonsCharacters);
+        }
+
+        public IActionResult CharacterDetails(int ID)
+        {
+            Character character = simpsonsCharacters.SingleOrDefault(c => c.ID == ID);
+            if (character == null)
+            {
+                return View("Error");
+            }
+            return View(character);
+        }
+
+        [HttpGet]
+        public IActionResult CharacterCreation()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CharacterCreation(Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                character.ID = currentId++;
+                simpsonsCharacters.Add(character);
+                return RedirectToAction("CharacterList");
+            }
+            return View(character);
+        }
+
+        [HttpGet]
+        public IActionResult CharacterEdit(int ID)
+        {
+            Character character = simpsonsCharacters.SingleOrDefault(c => c.ID == ID);
+            if (character == null)
+            {
+                return View("Error");
+            }
+            return View(character);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CharacterEdit(Character editedCharacter)
+        {
+            if (ModelState.IsValid)
+            {
+                Character characterToUpdate = simpsonsCharacters.FirstOrDefault(c => c.ID == editedCharacter.ID);
+                if (characterToUpdate != null)
+                {
+                    characterToUpdate.Name = editedCharacter.Name;
+                    characterToUpdate.Age = editedCharacter.Age;
+                    characterToUpdate.Job = editedCharacter.Job;
+                }
+                else
+                {
+                    return View("Error");
+                }
+                return RedirectToAction("CharacterList");
+            }
+            return View(editedCharacter);
+        }
+
+        public IActionResult CharacterDelete(int ID)
+        {
+            Character character = simpsonsCharacters.SingleOrDefault(c => c.ID == ID);
+            simpsonsCharacters.Remove(character);
+            return RedirectToAction("CharacterList");
+        }
+    }
+}
---
+++ Desarrollo entorno servidor (DSW)/Actividades/AUT02_03_AfonsoAlejandro/AUT02_03 The Simpsoms/Models/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AUT02_03_The_Simpsoms.Models
+{
+    public class Character
+    {
+        public int ID { get; set; }
+
+        [StringLength(15, MinimumLength =3, ErrorMessage = "El nombre tiene que tener entre 3 y 15 caracteres.")]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [DisplayName("Nombre")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria.")]
+        [Range(0, 100, ErrorMessage = "El valor de edad debe estar entre 0 y 100.")]
+        [DisplayName("Edad")]
+        public int Age { get; set; }
+
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "El trabajo tiene que tener entre 5 y 20 caracteres.")]
+        [Required(ErrorMessage = "Poner su trabajo es obligatorio.")]
+        [DisplayName("Trabajo")]
+        public string? Job { get; set; }
+    }
+}
