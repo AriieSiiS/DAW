720a150a223e800752a4d697bb249ec8117eca56
AriieSiiS <ariesix20@gmail.com>
2023-10-15 19:50:32 +0100

    DOR - DSW - Trabajo de casa
    - Proyecto de DOR empezado desde 0
    - Actividad de persistencia de datos de DSW

---
+++ Desarrollo entorno servidor (DSW)/Actividades/AUT02_03_TEST_AfonsoAlejandro/ASP.NET MVC - Persistencia/ASP.NET MVC - Persistencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ASP.NET_MVC___Persistencia.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ASPNET_MVC___PersistenciaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ASPNET_MVC___PersistenciaContext") ?? throw new InvalidOperationException("Connection string 'ASPNET_MVC___PersistenciaContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
