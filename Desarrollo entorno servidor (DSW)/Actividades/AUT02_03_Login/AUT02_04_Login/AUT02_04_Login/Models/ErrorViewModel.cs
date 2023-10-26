1c5748fd1a1970b60eeedac2f91c3f3f2632caf1
AriieSiiS <ariesix20@gmail.com>
2023-10-26 18:13:26 +0100

    DSW - Actividades

--- Desarrollo entorno servidor (DSW)/Actividades/AUT02_03_Login/AUT02_04_Login/AUT02_04_Login/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AUT02_04_Login.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Desarrollo entorno servidor (DSW)/Actividades/AUT02_05_Identity/AUT_04_LoginTrue/AUT_04_LoginTrue/Program.cs
@@ -0,0 +1,45 @@
+using AUT_04_LoginTrue.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("NorthwindContext") ?? throw new InvalidOperationException("NorthwindContext");
+
+builder.Services.AddDbContext<NorthwindContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = true;
+})
+
+    .AddEntityFrameworkStores<NorthwindContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+app.Run();
